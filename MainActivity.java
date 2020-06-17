8e6c3c3cdc73b2cfca40d7128259c41d0139ec75
Marcos Santos <49215259+1268marcos@users.noreply.github.com>
2020-06-17 17:37:53 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.conversortemperaturacf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditTextValue;
+
+    private TextView mTextViewResult;
+
+    private Button mButtonCalculate;
+
+    private void newCalculate(){
+        //  Fórmula
+        //(0 °C × 9/5) + 32 = 32 °F
+
+        if(TextUtils.isEmpty(mEditTextValue.getText().toString())) {
+            Toast.makeText(this, "Informe o valor da temperatura em graus celsius", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            double tempC = Double.parseDouble(mEditTextValue.getText().toString());
+            double tempF = (tempC * 9/5) + 32;
+            mTextViewResult.setText(Double.toString(tempF));
+        }
+
+    }
+
+    public class NewCalculateClick implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            newCalculate();
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditTextValue = findViewById(R.id.editText_Value);
+
+        mTextViewResult = findViewById(R.id.textView_Result);
+
+        mButtonCalculate = findViewById(R.id.button_Calculate);
+
+        mButtonCalculate.setOnClickListener(new NewCalculateClick());
+
+    }
+}
